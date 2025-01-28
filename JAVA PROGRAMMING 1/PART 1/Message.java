7912b902cb25a62dfaf19d7dce5f25f3c8d3b58e
Stefania ogun <stefania.ogun2@mail.dcu.ie>
2025-01-28 14:50:46 +0000

    bro

---
+++ JAVA PROGRAMMING 1/PART 1/Message.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Message{
+
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Write a message: ");
+		String Message = scanner.nextLine();
+		System.out.println(Message);
+}
+
+}
